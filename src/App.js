e251e995fe5ae9f071b1501366e4091b6574e89e
Taras <zaslavskiy.t.r@gmail.com>
2021-04-27 16:32:43 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+        <Navbar/>
+    </BrowserRouter>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
-import {LOGIN_ROUTE} from "./utils/const";
+import {CHAT_ROUTE, LOGIN_ROUTE} from "./utils/const";
 import Login from "./components/Login";
+import Chat from "./components/Chat";
 
 export const publicRoutes = [
   {
     path: LOGIN_ROUTE,
     Component: Login
+  }
+]
+
+export const privateRoutes = [
+  {
+    path: CHAT_ROUTE,
+    Component: Chat
   }
 ]
