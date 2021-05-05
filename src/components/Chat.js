4e8f52c131034026294a1e6739cfc094f0c4a08e
Taras <zaslavskiy.t.r@gmail.com>
2021-05-05 06:30:27 +0300

    finished

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,17 +1,38 @@
 import React, {useContext, useState} from 'react';
 import {Context} from "../index";
 import {useAuthState} from "react-firebase-hooks/auth";
-import {Button, Container, Grid, TextField} from "@material-ui/core";
+import {Avatar, Button, Container, Grid, TextField} from "@material-ui/core";
+import {useCollectionData} from "react-firebase-hooks/firestore";
+import Loader from "./Loader";
+import firebase from "firebase";
+
 
 const Chat = () => {
   const {auth, firestore} = useContext(Context)
   const [user] = useAuthState(auth)
 
-  const [value,setValue] = useState('')
+  const [value, setValue] = useState('')
 
-  const  sendMessage = async ()=>{
-    console.log(value);
+  const [messages, loading] = useCollectionData(
+    firestore.collection('messages').orderBy('createdAt')
+  )
 
+  const sendMessage = async () => {
+    // console.log(value);
+
+    firestore.collection('messages').add({
+      uid: user.uid,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      text: value,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
+    setValue('')
+  }
+
+  if (loading) {
+    return <Loader/>
   }
 
   return (
@@ -24,8 +45,28 @@ const Chat = () => {
       >
 
         <div style={{marginTop: "10px", width: "80%", height: "70vh", border: "1px solid grey", overflowY: "auto"}}>
+          {messages.map(message =>
+            <div style={{
+              margin:10,
+              border: user.uid===message.uid ? '2px solid gray': '2px dashed red',
+              marginLeft: user.uid===message.uid ? 'auto': '10px',
+              width: 'fit-content',
+              padding: 5
+
+
+            }}>
+              <Grid container>
+                <Avatar src={message.photoURL}/>
+                <p>{message.displayName}</p>
+              </Grid>
+              <p>{message.text}</p>
+
+            </div>
+
+          )}
 
         </div>
+
         <Grid container
               direction={"column"}
               alignItems={"flex-end"}
@@ -36,11 +77,11 @@ const Chat = () => {
             rowsMax={2}
             variant={"outlined"}
             value={value}
-            onChange={e=>setValue(e.target.value)}
+            onChange={e => setValue(e.target.value)}
 
           />
 
-          <Button onClick={()=>sendMessage()} variant={"outlined"}>Отправить</Button>
+          <Button onClick={() => sendMessage()} variant={"outlined"}>Отправить</Button>
 
         </Grid>
 
